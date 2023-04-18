258ff48fc1138a86e9e35925775925376d8562b0
Vasilii <petgga@gmail.com>
2023-04-18 22:32:03 +0300

    SignUpForm

---
+++ task_manager/users/forms.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.forms import UserCreationForm
+from task_manager.users.models import CustomUser
+
+
+class SignUpForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = ('first_name', 'last_name', 'username', 'password1', 'password2',)
