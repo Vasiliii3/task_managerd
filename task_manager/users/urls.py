fbf3272b9d859f80d363048a409c97090e65d08d
Vasilii <petgga@gmail.com>
2023-04-23 19:25:18 +0300

    add delete user

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from django.views.generic import TemplateView
-
+from task_manager.users.views import CreateUserView, LoginUserView, UsersView, \
+    UpdateUserView, DeleteUserView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name="users/users.html"), name='users_home'),
+    path('', UsersView.as_view(), name='users_home'),
+    path('create/', CreateUserView.as_view(), name='users_create'),
+    path('login/', LoginUserView.as_view(), name='users_login'),
+    path('<int:pk>/update/', UpdateUserView.as_view(), name='users_update'),
+    path('<int:pk>/delete/', DeleteUserView.as_view(), name='users_delete'),
 ]
--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,33 +1,65 @@
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView
 from django.contrib.messages.views import SuccessMessageMixin
-from django.contrib import messages
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
-from .models import CustomUser
-from .forms import SignUpForm
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView
+from task_manager.users.models import CustomUser
+from task_manager.users.forms import SignUpForm
 from django.utils.translation import gettext_lazy as _
+from task_manager.mixins import CustomLoginRequiredMixin, EditOwnAccountRequiredMixin
 
 
 # Create your views here.
+class UsersView(ListView):
+    model = CustomUser
+    template_name = 'users/users.html'
+    context_object_name = 'users'
+    ordering = ['id']
+    extra_context = {
+        'Description': _('Create user'),
+    }
 
 
 class CreateUserView(SuccessMessageMixin, CreateView):
     model = CustomUser
     form_class = SignUpForm
     template_name = 'forms.html'
-    success_url = reverse_lazy('login')
+    success_url = reverse_lazy('users_login')
+    success_message = _("Registration successful")
     extra_context = {
         'Description': _('Create user'),
         'Button': _('Register'),
     }
-    success_message = _("Registration successful")
 
 
 class LoginUserView(SuccessMessageMixin, LoginView):
     template_name = 'forms.html'
+    success_message = _('You are logged in!')
     extra_context = {
         'Description': _('Log In'),
-        'Button': _('Sign in'),
+        'Button': _('Enter'),
     }
-    success_message = _('You are logged in!')
 
+
+class ClassMixins(SuccessMessageMixin, CustomLoginRequiredMixin, EditOwnAccountRequiredMixin):
+    model = CustomUser
+    success_url = reverse_lazy('users_home')
+
+
+class UpdateUserView(ClassMixins, UpdateView):
+    form_class = SignUpForm
+    template_name = 'forms.html'
+    success_message = _("User is successfully updated")
+    extra_context = {
+        'Description': _('Update user'),
+        'Button': _('Change'),
+    }
+
+
+class DeleteUserView(ClassMixins, DeleteView):
+    template_name = 'users/delete.html'
+    success_message = _("User is successfully deleted")
+    context_object_name = 'user'
+    extra_context = {
+        'Description': _('Delete user'),
+    }
