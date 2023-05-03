3360fbfc2248a8b240687d2d49c886909a496735
Vasilii <petgga@gmail.com>
2023-05-03 22:46:55 +0300

    remove tag verbose_name from created_at

--- task_manager/statuses/models.py
+++ task_manager/statuses/models.py
@@ -5,7 +5,7 @@ from django.utils.translation import gettext_lazy as _
 # Create your models here.
 class Status(models.Model):
     name = models.CharField(max_length=255, unique=True, verbose_name=_('Name'))
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('Creation date'))
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
