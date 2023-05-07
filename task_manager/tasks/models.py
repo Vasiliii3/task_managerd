6a868f581339e09e65a0a1393a1f6084c6e4652d
Vasilii <petgga@gmail.com>
2023-05-07 20:43:00 +0300

    add on_delete=models.CASCADE to labels

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -9,15 +9,22 @@ from django.utils.translation import gettext_lazy as _
 class Task(models.Model):
     name = models.CharField(max_length=255, unique=True, verbose_name=_('Name'))
     description = models.TextField(blank=True, verbose_name=_('Description'))
-    status = models.OneToOneField(
+    status = models.ForeignKey(
         Status, on_delete=models.PROTECT, verbose_name=_('Status'), related_name='status')
-    author = models.OneToOneField(
+    author = models.ForeignKey(
         CustomUser, on_delete=models.PROTECT, related_name='author')
-    executor = models.OneToOneField(
-        CustomUser, on_delete=models.PROTECT, verbose_name=_('Executor'), related_name='executor')
+    executor = models.ForeignKey(
+        CustomUser, on_delete=models.PROTECT, null=True, blank=True, verbose_name=_('Executor'),
+        related_name='executor')
     labels = models.ManyToManyField(
-        Label, verbose_name=_('Labels'), related_name='labels')
+        Label, through='TaskLabels', through_fields=('task', 'label'),
+        blank=True, verbose_name=_('Labels'), related_name='labels')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
+
+
+class TaskLabels(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    label = models.ForeignKey(Label, on_delete=models.PROTECT)
