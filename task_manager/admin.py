673ac1939b4433f2b34b1115a26186cebb408fff
Vasilii <petgga@gmail.com>
2023-05-09 19:11:17 +0300

    add task

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 from task_manager.labels.models import Label
 from task_manager.statuses.models import Status
 from task_manager.users.models import CustomUser
+from task_manager.tasks.models import Task
 
 
 # Register your models here.
 
-@admin.register(Label, Status, CustomUser)
+@admin.register(Label, Status, CustomUser, Task)
 class MyTaskAdmin(admin.ModelAdmin):
     pass
