94b29bf7ded0bc61a43071ffaa36b2d55ad7d6e7
Vasilii <petgga@gmail.com>
2023-05-13 18:43:27 +0300

    add labels = django_filters.ModelChoiceFilter(

--- task_manager/tasks/filters.py
+++ task_manager/tasks/filters.py
@@ -3,12 +3,18 @@ from django import forms
 
 from task_manager.tasks.models import Task
 from django.utils.translation import gettext_lazy as _
+from task_manager.labels.models import Label
 
 
 class TaskFilter(django_filters.FilterSet):
     is_author = django_filters.BooleanFilter(
         field_name='author', label=_('Only your own tasks'),
         method='filter_is_author', widget=forms.CheckboxInput,
+    )
+
+    labels = django_filters.ModelChoiceFilter(
+        label=_('Label'),
+        queryset=Label.objects.all(),
     )
 
     class Meta:
